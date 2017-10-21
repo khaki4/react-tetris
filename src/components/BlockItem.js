21677ff3f98ce97491fb28027b02874222c28f95
정지원 <4dakto@gmail.com>
2017-10-21 20:42:45 +0900

    Jiwon :: block piece 생성

--- src/components/BlockItem.js
+++ src/components/BlockItem.js
@@ -1,10 +1,52 @@
 import React from "react";
 import { connect } from "react-redux";
 
+
+const BlockMold = (() => {
+  const moldSelector = [
+    [
+      0, 0, 0, 0,
+      0, 0, 0, 0,
+      1, 1, 1, 1,
+      0, 0, 0, 0,
+    ],
+    [
+      0, 0, 0, 0,
+      0, 1, 1, 0,
+      0, 1, 1, 0,
+      0, 0, 0, 0,
+    ],
+    [
+      0, 0, 0, 0,
+      0, 1, 0, 0,
+      1, 1, 1, 0,
+      0, 0, 0, 0,
+    ],
+    [
+      0, 0, 0, 0,
+      0, 1, 1, 0,
+      1, 1, 0, 0,
+      0, 0, 0, 0,
+    ]
+  ]
+  const pieceIndex = Math.round(Math.random() * moldSelector.length - 1)
+  const mold = moldSelector[pieceIndex]
+  return () => {
+    return (
+      <div className="blockmold_wrapper">
+        {mold.map((sector, index) => {
+          return <div key={index} className={`blockmold_unit ${sector ? 'filled' : ''}`}></div>
+        })}
+      </div>
+    )
+  }
+})()
+
+
 const BlockItem = ({position}) => {
   return (
     <div className="blockItem" style={position}>
-      item
+      <BlockMold />
     </div>
   )
 }
